f8fa7e0b1c27b96bc9a26b853c84dae2c51d8cc4
Alek Dussuau <ajdussuau@gmail.com>
2021-06-06 22:38:06 -0400

    Initial solution

--- src/main/java/Base/App.java
+++ src/main/java/Base/App.java
@@ -4,7 +4,53 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        String month = "";
+
+        System.out.print("PLease enter the number of the month: ");
+        String buff_num = input.nextLine();
+
+        int num = Integer.parseInt(buff_num);
+
+        switch (num) {
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+        }
+        System.out.println("The name of the month is " + month);
     }
 }
 
